49ff13732ec96f721602dea51b802dc380a3c9b5
Giorgio Salvemini <s351995@oslomet.no>
2023-01-22 17:05:02 +0000

    Update control_enhet/Logger.h

--- control_enhet/Logger.h
+++ control_enhet/Logger.h
@@ -10,10 +10,10 @@ namespace Logger
 
 enum
 {
-	LOG_INFO =			0x1, // general information
-	LOG_WARNING =		0x2, // can cause oddities
-	LOG_ERROR =			0x4, // fatal to an operation but not a program
-	LOG_FATAL =			0x8 // fatal to the whole program
+	LOG_INFO =		0x1, // general information
+	LOG_WARNING =	0x2, // can cause oddities
+	LOG_ERROR =		0x4, // fatal to an operation but not a program
+	LOG_FATAL =		0x8 // fatal to the whole program
 };
 
 static std::string getCurrentTime()
