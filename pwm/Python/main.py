146d101adea536c2924fef234b4538011322548f
s351995 <s351995@oslomet.no>
2023-01-26 16:06:18 +0100

    Upload

--- pwm/Python/main.py
+++ pwm/Python/main.py
@@ -1,10 +1,9 @@
 from time import sleep
 import board
 from adafruit_motorkit import MotorKit
-kit = MotorKit()
 
-kit.motor1.throttle = 1.0
-kit.motor2.throttle = 1.0
-sleep(0.5)
-kit.motor1.throttle = 0
-kit.motor2.throttle = 0
+kit = MotorKit(i2c = board.I2C())
+
+for i in range(1000):
+	kit.stepper1.onestep()
+	sleep(0.01)
