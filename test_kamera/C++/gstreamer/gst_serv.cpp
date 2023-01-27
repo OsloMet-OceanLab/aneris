8b014ca054249b6c3b67e5c148c77d66c3df98e8
Giorgio Salvemini <s351995@oslomet.no>
2023-01-27 13:25:42 +0000

    Update test_kamera/C++/gstreamer/gst_serv.cpp

--- test_kamera/C++/gstreamer/gst_serv.cpp
+++ test_kamera/C++/gstreamer/gst_serv.cpp
@@ -1,30 +1,27 @@
 #include <gst/gst.h>
 
 int main(int argc, char *argv[]) {
-  gst_init(&argc, &argv);
+    gst_init(&argc, &argv);
 
-  GstElement *pipeline, *source, *encoder, *payloader, *sink;
+    GstElement *pipeline, *src, *hls, *sink;
 
-  pipeline = gst_pipeline_new("webcam-streamer");
-  source = gst_element_factory_make("v4l2src", "webcam-source");
-  encoder = gst_element_factory_make("vp8enc", "encoder");
-  payloader = gst_element_factory_make("rtpvp8pay", "payloader");
-  sink = gst_element_factory_make("udpsink", "udp-sink");
+    pipeline = gst_pipeline_new("my-pipeline");
+    src = gst_element_factory_make("v4l2src", "webcam-src");
+    hls = gst_element_factory_make("hlssink", "hls-sink");
+    sink = gst_element_factory_make("filesink", "file-sink");
 
-  g_object_set(G_OBJECT(sink), "host", "127.0.0.1", NULL);
-  g_object_set(G_OBJECT(sink), "port", 5000, NULL);
+    g_object_set(G_OBJECT(src), "device", "/dev/video0", NULL);
+    g_object_set(G_OBJECT(hls), "location", "hls/stream.m3u8", NULL);
+    g_object_set(G_OBJECT(hls), "playlist-location", "hls/playlist.m3u8", NULL);
+    g_object_set(G_OBJECT(sink), "location", "hls/segment%05d.ts", NULL);
+    g_object_set(G_OBJECT(hls), "max-files", 5, NULL);
 
-  gst_bin_add_many(GST_BIN(pipeline), source, encoder, payloader, sink, NULL);
-  gst_element_link_many(source, encoder, payloader, sink, NULL);
+    gst_bin_add_many(GST_BIN(pipeline), src, hls, sink, NULL);
+    gst_element_link_many(src, hls, sink, NULL);
 
-  gst_element_set_state(pipeline, GST_STATE_PLAYING);
+    gst_element_set_state(pipeline, GST_STATE_PLAYING);
+    gst_element_get_state(pipeline, NULL, NULL, GST_CLOCK_TIME_NONE);
 
-  GMainLoop *loop = g_main_loop_new(NULL, FALSE);
-  g_main_loop_run(loop);
-
-  gst_element_set_state(pipeline, GST_STATE_NULL);
-  gst_object_unref(pipeline);
-
-  return 0;
+    return 0;
 }
 
