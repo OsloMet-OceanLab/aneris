6178067002499b1c60f09f05f23606e43a397860
s351995 <s351995@oslomet.no>
2023-01-30 15:45:23 +0100

    Upload

--- test_kamera/Python/mjpeg_recv.py
+++ test_kamera/Python/mjpeg_recv.py
@@ -2,18 +2,17 @@ from cv2 import VideoCapture, imshow, destroyAllWindows, waitKey
 from sys import exit
 
 def main():
-	cap = VideoCapture("http://localhost:5000/stream")
 	try:
-		#with VideoCapture('http://10.44.6.51:5000/stream') as cap:
-			if not cap.isOpened():
-				print('Unable to open stream')
-				exit(1)
-				
-			while True:
-				_, frame = cap.read()
-				imshow('frame', frame)
-				if waitKey(10) == 27:
-					break
+		cap = VideoCapture("http://localhost:5000/stream")
+		if not cap.isOpened():
+			print('Unable to open stream')
+			exit(1)
+			
+		while True:
+			_, frame = cap.read()
+			imshow('frame', frame)
+			if waitKey(10) == 27:
+				break
 	except Exception as e:
 		print(f'Exception: {str(e)}')
 	finally:
