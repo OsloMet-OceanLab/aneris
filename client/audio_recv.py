3a124f1e26679a0e7159695eccc66425c34c87d0
s351995 <s351995@oslomet.no>
2023-02-07 15:18:45 +0100

    UpUpdated audio_recv.py

--- client/audio_recv.py
+++ client/audio_recv.py
@@ -1,49 +1,61 @@
 from socket import socket, AF_INET, SOCK_DGRAM
-import sounddevice
+import sys, wave
 
 HOST, PORT = '', 5453
 
+d = b''
+i = 0
+
 with socket(AF_INET, SOCK_DGRAM) as sock:
     sock.bind((HOST, PORT))
-    d = []
-    i = 0
-    while True:
-        try:
+    try:
+        while True:
             i += 1
             message, address = sock.recvfrom(4096)
             sync = message.hex()[0:4]
-            print(f"Sync: {sync}")
+            #print(f"Sync: {sync}")
             size = int(message.hex()[4:8], 16)
-            print(f"Size: {size}")
+            #print(f"Size: {size}")
             if message.hex()[8:10] == '00':
                 dtype = 'Unknown'
             elif message.hex()[8:10] == '01':
                 dtype = 'DAQ'
             elif message.hex()[8:10] == '02':
                 dtype = 'Noise'
-            print(f"Data type: {dtype}")
-    
+            #print(f"Data type: {dtype}")
+
             data = message[5:size]
-    
+
             if data.hex()[0:2] == '00':
                 dformat = 'Unknown'
             elif data.hex()[0:2] == '01':
                 dformat = 'PCM16'
             elif data.hex()[0:2] == '02':
                 dformat = 'PCM24'
-            print(f"Data format: {dformat}")
+            #print(f"Data format: {dformat}")
             seq = data.hex()[2:6]
-            print(f"Sequence number: {seq}")
+            #print(f"Sequence number: {seq}")
             sr = int(data.hex()[6:14], 16)
-            print(f"Sample rate: {sr}")
+            #print(f"Sample rate: {sr}")
             chmap = int(data.hex()[14:22], 16)
-            print(f"Enabled channels: {chmap}")
+            #print(f"Enabled channels: {chmap}")
             scnt = int(data.hex()[22:26], 16)
-            print(f"Samples per channel: {scnt}")
+            #print(f"Samples per channel: {scnt}")
             raw = data[13:(3*scnt*chmap)+13]
-            print(raw.hex())
-            sounddevice.RawStream(samplerate=96_000, dtype=int16, blocksize=333, channels=1)
+            #print(raw.hex())
             
-        except KeyboardInterrupt:
-            print("Done")
-            break
+            file = b'RIFF 0413 WAVE fmt  0000 01 01 17700 192000 02 0f data 0000'
+            
+            d += raw
+            
+            if i % 1000 == 0:
+                break
+        
+        with wave.open('test.wav', 'wb') as wavfile:
+            wavfile.setparams((1, 2, 96_000, 0, 'NONE', 'NONE'))
+            wavfile.writeframes(d)
+        print(d)
+
+    except KeyboardInterrupt:
+        print("Done")
+            
