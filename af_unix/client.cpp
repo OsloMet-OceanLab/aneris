c51bf446fb22b4b6ff789470e7632f2db8dfc0ce
Giorgio Salvemini <s351995@oslomet.no>
2023-02-14 10:01:09 +0000

    Update af_unix/server.cpp, af_unix/client.cpp

--- af_unix/client.cpp
+++ af_unix/client.cpp
@@ -1,6 +1,64 @@
-#include <iostream>
+/************************************************************/
+/* This is a datagram socket client sample program for UNIX */
+/* domain sockets. This program creates a socket and sends  */
+/* data to a server.                                        */
+/************************************************************/
 
-int main()
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+
+#define SERVER_PATH "tpf_unix_sock.server"
+#define DATA "Hello from client\n"
+
+int main(void)
 {
-	return 0;
+    int client_socket, rc;
+    struct sockaddr_un remote; 
+    char buf[256];
+    memset(&remote, 0, sizeof(struct sockaddr_un));
+    
+    /****************************************/
+    /* Create a UNIX domain datagram socket */
+    /****************************************/
+    client_socket = socket(AF_UNIX, SOCK_DGRAM, 0);
+    if (client_socket == -1) {
+        printf("SOCKET ERROR = %d\n", sock_errno());
+        exit(1);
+    }
+
+    /***************************************/
+    /* Set up the UNIX sockaddr structure  */
+    /* by using AF_UNIX for the family and */
+    /* giving it a filepath to send to.    */
+    /***************************************/
+    remote.sun_family = AF_UNIX;        
+    strcpy(remote.sun_path, SERVER_PATH); 
+    
+    /***************************************/
+    /* Copy the data to be sent to the     */
+    /* buffer and send it to the server.   */
+    /***************************************/
+    strcpy(buf, DATA);
+    printf("Sending data...\n");
+    rc = sendto(client_socket, buf, strlen(buf), 0, (struct sockaddr *) &remote, sizeof(remote));
+    if (rc == -1) {
+        printf("SENDTO ERROR = %d\n", sock_errno());
+        close(client_sock);
+        exit(1);
+    }   
+    else {
+        printf("Data sent!\n");
+    }
+    
+    /*****************************/
+    /* Close the socket and exit */
+    /*****************************/
+    rc = close(client_sock);
+
+    return 0;
 }
--- af_unix/server.cpp
+++ af_unix/server.cpp
@@ -1,6 +1,75 @@
-#include <iostream>
+/************************************************************/
+/* This is a datagram socket server sample program for UNIX */
+/* domain sockets. This program creates a socket and        */
+/* receives data from a client.                             */
+/************************************************************/
 
-int main()
-{
-	return 0;
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+
+#define SOCK_PATH "tpf_unix_sock.server"
+
+int main(void){
+
+    int server_sock, len, rc;
+    int bytes_rec = 0;
+    struct sockaddr_un server_sockaddr, peer_sock;
+    char buf[256];
+    memset(&server_sockaddr, 0, sizeof(struct sockaddr_un));
+    memset(buf, 0, 256);
+    
+    /****************************************/
+    /* Create a UNIX domain datagram socket */
+    /****************************************/
+    server_sock = socket(AF_UNIX, SOCK_DGRAM, 0);
+    if (server_sock == -1){
+        printf("SOCKET ERROR = %d", sock_errno());
+        exit(1);
+    }
+    
+    /***************************************/
+    /* Set up the UNIX sockaddr structure  */
+    /* by using AF_UNIX for the family and */
+    /* giving it a filepath to bind to.    */
+    /*                                     */
+    /* Unlink the file so the bind will    */
+    /* succeed, then bind to that file.    */
+    /***************************************/
+    server_sockaddr.sun_family = AF_UNIX;
+    strcpy(server_sockaddr.sun_path, SOCK_PATH); 
+    len = sizeof(server_sockaddr);
+    unlink(SOCK_PATH);
+    rc = bind(server_sock, (struct sockaddr *) &server_sockaddr, len);
+    if (rc == -1){
+        printf("BIND ERROR = %d", sock_errno());
+        close(server_sock);
+        exit(1);
+    }
+    
+    /****************************************/
+    /* Read data on the server from clients */
+    /* and print the data that was read.    */
+    /****************************************/
+    printf("waiting to recvfrom...\n");
+    bytes_rec = recvfrom(server_sock, buf, 256, 0, (struct sockaddr *) &peer_sock, &len);
+    if (bytes_rec == -1){
+        printf("RECVFROM ERROR = %d", sock_errno());
+        close(server_sock);
+        exit(1);
+    }
+    else {
+       printf("DATA RECEIVED = %s\n", buf);
+    }
+    
+    /*****************************/
+    /* Close the socket and exit */
+    /*****************************/
+    close(server_sock);
+
+    return 0;
 }
