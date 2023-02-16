a9a2a1056402949685e92296751b3139d3faad54
Giorgio Salvemini <s351995@oslomet.no>
2023-02-16 15:42:37 +0000

    Update threading/main.cpp

--- threading/main.cpp
+++ threading/main.cpp
@@ -1,13 +1,32 @@
 #include <iostream>
 #include <pthread.h>
+#include <unistd.h>
 
-void *fun(int param)
+void *fun(void *arg)
 {
-	printf("in thread, param: %d", param);
-	return &param;
+        int ret = 7 + *(int*)arg, i = 0;
+        printf("in thread, param: %d\n", *(int*)arg);
+        while(true)
+        {
+                printf("in thread, i: %d\n", i++);
+                ret += i;
+                sleep(1);
+        }
+
+        pthread_exit(&ret); // ret becomes available by using pthread_join()
 }
 
 int main()
 {
-	pthread_t t;
+        pthread_t t;
+        int ret, arg = 7;
+
+        ret = pthread_create(&t, NULL, &fun, &arg);
+
+        sleep(10);
+        pthread_cancel(t); // effectively ends thread
+
+        printf("cancelled thread, ret: %d\n", ret);
+
+        return 0;
 }
