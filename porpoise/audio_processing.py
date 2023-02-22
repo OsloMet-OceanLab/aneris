72c27a1fa44e5e642c2a9bd61ecba859386f6abb
Giorgio Salvemini <s351995@oslomet.no>
2023-02-22 10:43:42 +0000

    Update porpoise/audio_processing.py

--- porpoise/audio_processing.py
+++ porpoise/audio_processing.py
@@ -8,16 +8,16 @@ Created on Wed Feb 22 10:14:30 2023
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.io.wavfile import read, write
-from IPython.display import Audio
+#from IPython.display import Audio
 from numpy.fft import fft, ifft
 
-Fs, data = read('C:\\Users\\OceanLab\\Onedrive - OsloMet\\Skrivebord\\test2.wav')
+rate, data = read('C:\\Users\\OceanLab\\Onedrive - OsloMet\\Skrivebord\\test2.wav')
 
 #data = data[:,0] # make audio mono, not needed here
 
-print(f'Sampling frequency is {Fs}')
+print(f'Sampling frequency is {rate}')
 
-Audio(data, rate=Fs)
+#Audio(data, rate=Fs)
 
 plt.figure()
 plt.plot(data)
@@ -26,3 +26,15 @@ plt.ylabel('Amplitude')
 plt.title('Waveform')
 plt.show()
 
+import noisereduce as nr
+
+reduced_noise = nr.reduce_noise(y=data, sr=rate)
+
+plt.figure()
+plt.plot(reduced_noise)
+plt.xlabel('Sample index')
+plt.ylabel('Amplitude')
+plt.title('Waveform (reduced)')
+plt.show()
+
+write('C:\\Users\\OceanLab\\Onedrive - OsloMet\\Skrivebord\\test4.wav', rate, reduced_noise)
