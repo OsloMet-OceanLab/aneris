18f8c621f84370fc7e5b838e35c5acf760690658
Giorgio Salvemini <s351995@oslomet.no>
2023-02-22 12:47:09 +0000

    Update porpoise/audio_recv.py

--- porpoise/audio_recv.py
+++ porpoise/audio_recv.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Feb 21 13:32:39 2023
+Created on Wed Feb 22 10:11:51 2023
 
 @author: OceanLab
 """
@@ -13,6 +13,42 @@ HOST, PORT = '', 5453
 d = b''
 i = 0
 raw_size = 0
+
+def parse(sock):
+    message, address = sock.recvfrom(1536)
+    sync = message.hex()[0:4]
+    #print(f"Sync: {sync}")
+    size = int(message.hex()[4:8], 16)
+    #print(f"Size: {size}")
+    if message.hex()[8:10] == '00':
+        dtype = 'Unknown'
+    elif message.hex()[8:10] == '01':
+        dtype = 'DAQ'
+    elif message.hex()[8:10] == '02':
+        dtype = 'Noise'
+    #print(f"Data type: {dtype}")
+
+    data = message[5:size]
+
+    if data.hex()[0:2] == '00':
+        dformat = 'Unknown'
+    elif data.hex()[0:2] == '01':
+        dformat = 'PCM16'
+    elif data.hex()[0:2] == '02':
+        dformat = 'PCM24'
+    #print(f"Data format: {dformat}")
+    seq = data.hex()[2:6]
+    #print(f"Sequence number: {seq}")
+    sr = int(data.hex()[6:14], 16)
+    #print(f"Sample rate: {sr}")
+    chmap = int(data.hex()[14:22], 16)
+    #print(f"Enabled channels: {chmap}")
+    scnt = int(data.hex()[22:26], 16)
+    #print(f"Samples per channel: {scnt}")
+    raw = data[13:(3*scnt*chmap)+13]
+    #print(raw.hex())
+    
+    return raw, scnt
 
 def genHeader(sampleRate, bitsPerSample, channels, samples):
     datasize = samples #* channels * bitsPerSample // 8
@@ -55,46 +91,35 @@ with socket(AF_INET, SOCK_DGRAM) as sock:
     sock.bind((HOST, PORT))
     print("Bound")
     try:
+        tmp = b''
+        tmp_size = 0
         while True:
-            i += 1
             if i % 100 == 0:
                 print(i)
-            message, address = sock.recvfrom(4096)
-            sync = message.hex()[0:4]
-            #print(f"Sync: {sync}")
-            size = int(message.hex()[4:8], 16)
-            #print(f"Size: {size}")
-            if message.hex()[8:10] == '00':
-                dtype = 'Unknown'
-            elif message.hex()[8:10] == '01':
-                dtype = 'DAQ'
-            elif message.hex()[8:10] == '02':
-                dtype = 'Noise'
-                continue
-            #print(f"Data type: {dtype}")
-
-            data = message[5:size]
-
-            if data.hex()[0:2] == '00':
-                dformat = 'Unknown'
-            elif data.hex()[0:2] == '01':
-                dformat = 'PCM16'
-            elif data.hex()[0:2] == '02':
-                dformat = 'PCM24'
-            #print(f"Data format: {dformat}")
-            seq = data.hex()[2:6]
-            #print(f"Sequence number: {seq}")
-            sr = int(data.hex()[6:14], 16)
-            #print(f"Sample rate: {sr}")
-            chmap = int(data.hex()[14:22], 16)
-            #print(f"Enabled channels: {chmap}")
-            scnt = int(data.hex()[22:26], 16)
-            #print(f"Samples per channel: {scnt}")
-            raw = data[13:(3*scnt*chmap)+13]
-            #print(raw.hex())
             
-            d += raw
-            raw_size += scnt * 3
+            if i % 6 == 0:
+                d = tmp
+                raw_size = tmp_size
+                tmp_size = 0
+                tmp = b''
+                print(raw_size)
+                
+                import matplotlib.pyplot as plt
+                
+                plt.figure()
+                plt.plot(d)
+                plt.xlabel('Sample index')
+                plt.ylabel('Amplitude')
+                plt.title('Waveform')
+                plt.show()
+                
+                sys.exit(0)
+            
+            tmp, scnt = parse(sock)
+            tmp_size += scnt * 3
+            #d += raw
+            #raw_size += scnt * 3
+            i += 1
             
             if i % 500 == 0:
                 print(raw_size)
