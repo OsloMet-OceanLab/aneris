142f5dff3526fd2408393c40986c4015da696868
Giorgio Salvemini <s351995@oslomet.no>
2023-02-27 10:42:04 +0000

    Update porpoise/fourier.py

--- porpoise/fourier.py
+++ porpoise/fourier.py
@@ -19,15 +19,22 @@ from scipy import stats
 import scipy
 
 plt.style.use('ggplot')
-plt.rcParams['font.family'] = 'sans-serif'
-plt.rcParams['font.serif'] = 'Ubuntu'
-plt.rcParams['font.monospace'] = 'Ubuntu Mono'
-plt.rcParams['font.size'] = 14
-plt.rcParams['axes.labelsize'] = 12
-plt.rcParams['xtick.labelsize'] = 12
-plt.rcParams[]
-plt.rcParams[]
-plt.rcParams[]
-plt.rcParams[]
-plt.rcParams[]
-plt.rcParams[]
+plt.rcParams['font.family'] = 'sans-serif' 
+plt.rcParams['font.serif'] = 'Ubuntu' 
+plt.rcParams['font.monospace'] = 'Ubuntu Mono' 
+plt.rcParams['font.size'] = 14 
+plt.rcParams['axes.labelsize'] = 12 
+plt.rcParams['axes.labelweight'] = 'bold' 
+plt.rcParams['axes.titlesize'] = 12 
+plt.rcParams['xtick.labelsize'] = 12 
+plt.rcParams['ytick.labelsize'] = 12 
+plt.rcParams['legend.fontsize'] = 12 
+plt.rcParams['figure.titlesize'] = 12 
+plt.rcParams['image.cmap'] = 'jet' 
+plt.rcParams['image.interpolation'] = 'none' 
+plt.rcParams['figure.figsize'] = (12, 10) 
+plt.rcParams['axes.grid']=True
+plt.rcParams['lines.linewidth'] = 2 
+plt.rcParams['lines.markersize'] = 8
+colors = ['xkcd:pale orange', 'xkcd:sea blue', 'xkcd:pale red', 'xkcd:sage green', 'xkcd:terra cotta', 'xkcd:dull purple', 'xkcd:teal', 'xkcd: goldenrod', 'xkcd:cadet blue',
+'xkcd:scarlet']
