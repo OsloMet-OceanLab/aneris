1ef1133212cb866dc73951b551bc6227fe90cb26
s351995 <s351995@oslomet.no>
2023-02-27 11:41:24 +0100

    Automatic upload

---
+++ porpoise/audio_recv_and_process.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 27 10:58:16 2023
+
+@author: salve
+"""
+
+from socket import socket, AF_INET, SOCK_DGRAM
+import sys
+
+def genHeader(sampleRate, bitsPerSample, channels, samples):
+    datasize = samples #* channels * bitsPerSample // 8
+    endian = 'little'
+    sign = True
+    o = bytes("RIFF", 'ascii')
+    o += (datasize + 36).to_bytes(4, endian, signed = sign)
+    o += bytes("WAVE", 'ascii')
+    o += bytes("fmt ", 'ascii')
+    o += (16).to_bytes(4, endian, signed = sign)
+    o += (1).to_bytes(2, endian, signed = sign)
+    o += (channels).to_bytes(2, endian, signed = sign)
+    o += (sampleRate).to_bytes(4, endian, signed = sign)
+    o += (sampleRate * channels * bitsPerSample // 8).to_bytes(4, endian, signed = sign)
+    o += (channels * bitsPerSample // 8).to_bytes(2, endian, signed = sign)
+    o += (bitsPerSample).to_bytes(2, endian, signed = sign)
+    o += bytes("data", 'ascii')
+    o += (datasize).to_bytes(4, endian, signed = sign)
+    return o
+
+HOST, PORT = '', 5453
+
+d = b''
+i = 0
+raw_size = 0
+
+with socket(AF_INET, SOCK_DGRAM) as sock:
+    sock.bind((HOST, PORT))
+    print("Bound")
+    try:
+        while True:
+            message, address = sock.recvfrom(1536)
+            sync = message.hex()[0:4]
+            #print(f"Sync: {sync}")
+            size = int(message.hex()[4:8], 16)
+            #print(f"Size: {size}")
+            if message.hex()[8:10] == '00':
+                dtype = 'Unknown'
+            elif message.hex()[8:10] == '01':
+                dtype = 'DAQ'
+            elif message.hex()[8:10] == '02':
+                dtype = 'Noise'
+            #print(f"Data type: {dtype}")
+
+            data = message[5:size]
+
+            if data.hex()[0:2] == '00':
+                dformat = 'Unknown'
+            elif data.hex()[0:2] == '01':
+                dformat = 'PCM16'
+            elif data.hex()[0:2] == '02':
+                dformat = 'PCM24'
+            #print(f"Data format: {dformat}")
+            seq = data.hex()[2:6]
+            #print(f"Sequence number: {seq}")
+            sr = int(data.hex()[6:14], 16)
+            #print(f"Sample rate: {sr}")
+            chmap = int(data.hex()[14:22], 16)
+            #print(f"Enabled channels: {chmap}")
+            scnt = int(data.hex()[22:26], 16)
+            #print(f"Samples per channel: {scnt}")
+            raw = data[13:(3*scnt*chmap)+13]
+            #print(raw.hex())
+            
+            d += raw
+            raw_size += scnt * 3
+            i += 1
+            
+            if i % 100 == 0:
+                print(i)
+
+            if i % 1000 == 0:
+                print(raw_size)
+                header = genHeader(96_000, 16, 1, raw_size)
+                wavfile = header + d
+                
+                break
+            
+        with open('test2.wav', 'wb') as stream:
+            stream.write(wavfile)
+
+    except KeyboardInterrupt:
+        print("Done")
+            
---
+++ porpoise/fourier.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 27 11:07:57 2023
+
+@author: salve
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import datetime
+import pywt
+#from scipy import signal
+from scipy.optimize import curve_fit
+import seaborn as sns
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
+from scipy import stats
+import scipy
+
+plt.style.use('ggplot')
+plt.rcParams['font.family'] = 'sans-serif'
+plt.rcParams['font.serif'] = 'Ubuntu'
+plt.rcParams['font.monospace'] = 'Ubuntu Mono'
+plt.rcParams['font.size'] = 14
+plt.rcParams['axes.labelsize'] = 12
+plt.rcParams['xtick.labelsize'] = 12
+plt.rcParams[]
+plt.rcParams[]
+plt.rcParams[]
+plt.rcParams[]
+plt.rcParams[]
+plt.rcParams[]
