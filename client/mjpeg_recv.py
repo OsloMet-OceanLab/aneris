466699451a66059433c72755ebe276674e5e92a9
Giorgio Salvemini <s351995@oslomet.no>
2023-02-28 20:43:30 +0000

    Update client/mjpeg_recv.py

--- client/mjpeg_recv.py
+++ client/mjpeg_recv.py
@@ -1,5 +1,7 @@
 from cv2 import VideoCapture, imshow, destroyAllWindows, waitKey
 
+import cv2 # temporary, better to just import individual classes/functions
+
 class Video(VideoCapture):
 	def __enter__(self):
 		return self
@@ -11,11 +13,38 @@ def main():
 		with Video("http://localhost:5000/stream") as cap:
 			if not cap.isOpened():
 				raise Exception('Unable to open stream')
+
 			while True:
 				_, frame = cap.read()
+				
+				# below is a code example to find squares from https://www.tutorialspoint.com/how-to-detect-a-rectangle-and-square-in-an-image-using-opencv-python
+
+				gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+				ret, thresh = cv2.threshold(gray, 50, 255, 0)
+				contours, hierarchy = cv2.findContours(thresh, 1, 2)
+
+				print("Number of contours detected:", len(contours))
+
+				for cnt in contours:
+					x1, y1 = cnt[0][0]
+					approx = cv2.approxPolyDP(cnt, 0.01 * cv2.arcLength(cnt, True), True)
+
+					if len(approx) == 4:
+						x, y, w, h = cv2.boundingRect(cnt)
+						ratio = float(w)/h
+
+						if 0.9 <= ratio <= 1.1:
+							frame = cv2.drawContours(frame, [cnt], -1, (0, 255, 255), 3)
+							cv2.putText(frame, 'Square', (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 0), 2)
+
+						else:
+							cv2.putText(frame, 'Rectangle', (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
+							frame = cv2.drawContours(frame, [cnt], -1, (0,255,0), 3)
+
 				imshow('frame', frame)
 				if waitKey(10) == 27:
 					break
+						
 	except Exception as e:
 		print(f'Exception: {str(e)}')
 	finally:
