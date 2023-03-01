233a332eaafe6c717a2cfda93966e4f003186a95
s351995 <s351995@oslomet.no>
2023-03-01 13:46:18 +0100

    Automatic upload

--- porpoise/24to32/24to32.cpp
+++ porpoise/24to32/24to32.cpp
@@ -10,10 +10,10 @@ static inline int32_t _24to32(std::array<uint8_t, 3> byteArray)
             ((int32_t)(byteArray[2]) << 8)) >> 8;
 }
 
-byte* parseNum(byte* buf, size_t len)
+byte* parseNum(const byte* buf, size_t len)
 {
-    byte* tmpbuf = (byte*)malloc(sizeof(byte)*4*len/3);
     std::array<uint8_t, 3> tmparr;
+    byte* newbuf = (byte*)malloc(sizeof(byte)*4*len/3);
 
     for (size_t i = 0; i < len; i += 3)
     {
@@ -23,10 +23,10 @@ byte* parseNum(byte* buf, size_t len)
 
         int32_t tmpint = _24to32(tmparr);
 
-        tmpbuf[i] =     (byte) (tmpint & (0xF << 12));  //  1111000000000000
-        tmpbuf[i+1] =   (byte) (tmpint & (0xF << 8));   //  0000111100000000
-        tmpbuf[i+2] =   (byte) (tmpint & (0xF << 4));   //  0000000011110000
-        tmpbuf[i+3] =   (byte) (tmpint & (0xF));        //  0000000000001111
+        newbuf[i] =     (byte) (tmpint & (0xFF << 24));
+        newbuf[i+1] =   (byte) (tmpint & (0xFF << 16));
+        newbuf[i+2] =   (byte) (tmpint & (0xFF << 8));
+        newbuf[i+3] =   (byte) (tmpint & (0xFF << 0));
     }
-    return tmpbuf;
+    return newbuf;
 }
