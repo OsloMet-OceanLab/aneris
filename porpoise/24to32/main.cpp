a564d306ff9e756cd205d0d8bf8d0860061d49a9
s351995 <s351995@oslomet.no>
2023-03-01 14:29:34 +0100

    Upload

--- porpoise/24to32/main.cpp
+++ porpoise/24to32/main.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
+#include <cstdlib>
 #include "24to32.hpp"
 
 int main()
 {
+    byte *buffer = (byte*)malloc(sizeof(byte)*1017);
+    File* fptr = fopen("../packet", "rb");
+    if(!fptr)
+    {
+        fprintf(stderr, "Couldn't open file");
+        exit(1);
+    }
+    fread(buffer, 1017, 1, fptr);
+
+    printf("%02X %02X", (uint8_t)buffer[0], (uint8_t)buffer[0]);
+
     return 0;
 }
