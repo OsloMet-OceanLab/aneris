5030f29feb801bdcd361135883655743c444d6c2
Giorgio Salvemini <s351995@oslomet.no>
2023-03-02 20:39:53 +0000

    Update porpoise/24to32/24to32.cpp, porpoise/24to32/24to32.hpp

--- porpoise/24to32/24to32.cpp
+++ porpoise/24to32/24to32.cpp
@@ -7,31 +7,24 @@
 namespace _24bit_converter
 {
 
-static inline int16_t _24to16(const byte byteArray[], bool invertEndianness)
+static inline int16_t _24to16(const byte byteArray[])
 {
-    return invertEndianness ?
-            (((uint16_t) (byteArray[2] << 8)) | (uint8_t) byteArray[1]) :
-            (((uint16_t) (byteArray[1] << 8)) | (uint8_t) byteArray[2]);
+    return (((uint16_t) (byteArray[1] << 8)) | (uint8_t) byteArray[2]);
 }
 
-byte* numto16(const byte* buf, size_t len, bool invertEndianness)
+byte* numto16(const byte *buf, size_t len, bool invertEndianness)
 {
-    std::vector<int16_t> tmparr2;
+    std::vector<int16_t> tmp_arr;
 
     byte* newbuf = (byte*)malloc(sizeof(byte)*2*len/3);
 
     for (size_t i = 0; i < len; i += 3)
-        tmparr2.push_back( invertEndianness ?
-                          (((uint16_t) (buf[i+2] << 8)) | (uint8_t) buf[i+1]) :
-                          (((uint16_t) (buf[i+1] << 8)) | (uint8_t) buf[i+2])
-                          );
-
-        //tmparr2.push_back(_24to16(buf+i), invertEndianness);
+        tmp_arr.push_back(tmparr2.push_back(_24to16(buf+i)));
 
     for (size_t i = 0; i < 2*len/3; i += 2)
     {
-        *(newbuf + i) =       0xFF & (tmparr2[i/2] >> 8);
-        *(newbuf + i + 1) =   0xFF & (tmparr2[i/2]);
+        *(newbuf + i) =       0xFF & (tmp_arr[i/2] >> (invertEndianness ? 0 : 8));
+        *(newbuf + i + 1) =   0xFF & (tmp_arr[i/2] >> (invertEndianness ? 8 : 0));
     }
     return newbuf;
 }
@@ -43,7 +36,7 @@ static inline int32_t _24to32(std::array<uint8_t, 3> byteArray)
             ((int32_t)(byteArray[2]) << 8)) >> 8;
 }
 
-byte* numto32(const byte* buf, size_t len, bool invertEndianness)
+byte* numto32(const byte *buf, size_t len, bool invertEndianness)
 {
     std::array<uint8_t, 3> tmparr;
     std::vector<int32_t> tmparr2;
@@ -61,10 +54,10 @@ byte* numto32(const byte* buf, size_t len, bool invertEndianness)
 
     for (size_t i = 0; i < 4*len/3; i += 4)
     {
-        *(newbuf + i) =       0xFF & (tmparr2[i/4] >> 24);
-        *(newbuf + i + 1) =   0xFF & (tmparr2[i/4] >> 16);
-        *(newbuf + i + 2) =   0xFF & (tmparr2[i/4] >> 8);
-        *(newbuf + i + 3) =   0xFF & (tmparr2[i/4]);
+        *(newbuf + i) =       0xFF & (tmparr2[i/4] >> (invertEndianness ? 0 : 24));
+        *(newbuf + i + 1) =   0xFF & (tmparr2[i/4] >> (invertEndianness ? 8 : 16));
+        *(newbuf + i + 2) =   0xFF & (tmparr2[i/4] >> (invertEndianness ? 16 : 8));
+        *(newbuf + i + 3) =   0xFF & (tmparr2[i/4] >> (invertEndianness ? 24 : 0));
     }
     return newbuf;
 }
--- porpoise/24to32/24to32.hpp
+++ porpoise/24to32/24to32.hpp
@@ -9,7 +9,7 @@ namespace _24bit_converter
 {
 
 byte* numto16(const byte *buf, size_t len, bool invertEndianness);
-byte* numto32(const byte* buf, size_t len, bool invertEndianness);
+byte* numto32(const byte *buf, size_t len, bool invertEndianness);
 
 } // end namespace _24bit_converter
 
