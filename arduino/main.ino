b5a8834d982265736ab406711a1aad351e9e424b
Giorgio Salvemini <s351995@oslomet.no>
2023-03-31 12:56:50 +0000

    Update arduino/main.ino

--- arduino/main.ino
+++ arduino/main.ino
@@ -1,9 +1,20 @@
 #include <Stepper.h>
 
-#define stepsPerRevolution 200 // change this to fit the number of steps per revolution for your motor
+#define STEPS_PER_REVOLUTION 200 // change this to fit the number of steps per revolution for your motor
+
+// gpio pins
+#define REL_1 6
+#define REL_2 7
+#define PI_1  8
+
+// motor pins
+#define M_1 9
+#define M_2 10
+#define M_3 11
+#define M_4 12
 
 // initialize the stepper library on pins 9 through 12:
-Stepper stepper(stepsPerRevolution, 9, 10 , 11, 12);
+Stepper stepper(STEPS_PER_REVOLUTION, M_1, M_2, M_3, M_4);
 
 void setup()
 {
@@ -14,34 +25,32 @@ void setup()
     Serial.begin(9600);
 
     // initialize arduino read pin
-    pinMode(5, INPUT);
+    pinMode(PI_1, INPUT);
 
     // initialize relay pins
-    pinMode(6, OUTPUT);
-    digitalWrite(6, LOW);
-    pinMode(7, OUTPUT);
-    digitalWrite(7, LOW);
+    pinMode(REL_1, OUTPUT);
+    digitalWrite(REL_1, LOW);
+    pinMode(REL_2, OUTPUT);
+    digitalWrite(REL_2, LOW);
 }
 
 void loop()
 {
-    if(digitalRead(5))
+    if(digitalRead(5) || 1)
     {
         // enable motors and drivers
-        digitalWrite(6, HIGH);
-        digitalWrite(7, HIGH);
+        digitalWrite(REL_1, HIGH);
+        digitalWrite(REL_2, HIGH);
 
         delay(1000); // 1s delay, in case the drivers need a second to boot properly
 
-        stepper.step(800);
+        stepper.step(1600);
         delay(100);
         stepper.step(-1600);
         delay(100);
-        stepper.step(800);
-        delay(100);
 
         // disable motors and drivers
-        digitalWrite(6, LOW);
-        digitalWrite(7, LOW);
+        digitalWrite(REL_1, LOW);
+        digitalWrite(REL_2, LOW);
     }
 }
