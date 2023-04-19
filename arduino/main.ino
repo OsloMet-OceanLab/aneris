f8132b0a084d2084da2d2507573f849713d75119
Giorgio Salvemini <s351995@oslomet.no>
2023-04-19 13:34:48 +0000

    Change to servos

--- arduino/main.ino
+++ arduino/main.ino
@@ -1,68 +1,34 @@
-#include <Stepper.h>
+#include <Servo.h>
 
-#define STEPS_PER_REVOLUTION 200 // change this to fit the number of steps per revolution for your motor
+Servo myservo; 
+Servo myservo2;
 
-// gpio pins
-#define REL_1 6
-#define REL_2 7
-#define PI_1  8
+int pos = 0;
 
-// motor pins
-#define M_1 9
-#define M_2 10
-#define M_3 11
-#define M_4 12
-
-// initialize the stepper library on pins 9 through 12:
-Stepper stepper(STEPS_PER_REVOLUTION, M_1, M_2, M_3, M_4);
-
-void setup()
-{
-    // set the speed at 100 rpm:
-    stepper.setSpeed(100);
-
-    // initialize the serial port:
-    Serial.begin(9600);
-
-    // initialize arduino read pin
-    pinMode(PI_1, INPUT);
-
-    // initialize relay pins
-    digitalWrite(REL_1, LOW);
-    digitalWrite(REL_2, LOW);
-
-    pinMode(REL_1, OUTPUT);
-    pinMode(REL_2, OUTPUT);
-
-    // initialize builtin led
-    pinMode(LED_BUILTIN, OUTPUT);
-    digitalWrite(LED_BUILTIN, LOW);
+void setup() {
+  // The servo control wire is connected to Arduino D2 pin.
+  myservo.attach(11);
+  myservo2.attach(12);
+  // Servo is stationary.
+  myservo.write(90);
+  myservo2.write(90);
 }
 
-void loop()
-{
-    if(digitalRead(PI_1))
-    {
-        digitalWrite(LED_BUILTIN, HIGH);
-        
-        // enable motors and drivers
-        digitalWrite(REL_1, HIGH);
-        digitalWrite(REL_2, HIGH);
-
-        delay(1000); // 1s delay, in case the drivers need a second to boot properly
-
-        // N.B. 2600 steps are one half rotation in our scenario
-        stepper.step(2600);
-        delay(100);
-        stepper.step(-2600);
-        delay(100);
-
-        digitalWrite(LED_BUILTIN, LOW);
-
-        // disable motors and drivers
-        digitalWrite(REL_1, LOW);
-        digitalWrite(REL_2, LOW);
-        delay(300000); // wait 5 minutes until next iteration
-    }
-    else delay(15000);
+void loop() {
+  // Servo spins forward at full speed for 1 second.
+  myservo.write(180);
+  myservo2.write(0);
+  delay(1000);
+  // Servo is stationary for 1 second.
+  myservo.write(90);
+  myservo2.write(90);
+  delay(1000);
+  // Servo spins in reverse at full speed for 1 second.
+  myservo.write(0);
+  myservo2.write(180);
+  delay(1000);
+  // Servo is stationary for 1 second.
+  myservo.write(90);
+  myservo2.write(90);
+  delay(1000);
 }
