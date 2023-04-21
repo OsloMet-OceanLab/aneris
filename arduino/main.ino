72ab3d7827dbbd0041b1c12b674c0b460460061c
Henrik Reimers <s352109@oslomet.no>
2023-04-21 14:38:37 +0000

    Update arduino/main.ino

--- arduino/main.ino
+++ arduino/main.ino
@@ -1,59 +1,46 @@
 #include <Servo.h>
 
-Servo myservo; 
-Servo myservo2;
+Servo servo[2];
 
-int hallSensorPin_1 = 2;
-int hallSensorPin_2 = 3;
-int hallSensorValue_1 = 0;
-int hallSensorValue_2 = 0;
+#define HALL_1 A7 // has led
+#define HALL_2 A6 // does not have led
+#define SERVO_1 11
+#define SERVO_2 12
 
 void setup() {
   Serial.begin(9600);
-
-  // The servo control wire is connected to Arduino D2 pin.
-  myservo.attach(11);
-  myservo2.attach(12);
-  // Servo is stationary.
-  myservo.write(90);
-  myservo2.write(90);
+  // The servo control wires are connected to Arduino D11-D12 pins.
+  servo[0].attach(SERVO_1);
+  servo[1].attach(SERVO_2);
+  
+  // Servos are stationary.
+  servo[0].write(90);
+  servo[1].write(90);
 
   // Enabling the digital pins on the hall sensor
-  pinMode(hallSensorPin_1, INPUT);
-  pinMode(hallSensorPin_2, INPUT);
+  pinMode(HALL_1, INPUT);
+  pinMode(HALL_2, INPUT);
 }
 
 void loop() {
-  Serial.print("hallSensorValue_1: ");
-  Serial.println(hallSensorValue_1);
-  Serial.print("hallSensorValue_2: ");
-  Serial.println(hallSensorValue_2);
+  servo[0].write(0);
+  servo[1].write(180);
 
-  // Servo spins forward at full speed.
-  myservo.write(0);
-  myservo2.write(180);
-  Serial.println("Spinning forward!");
- 
-  while(!digitalRead(hallSensorPin_1));
-  Serial.println("Stopped!");
-  myservo.write(90);
-  myservo2.write(90);
+  while(analogRead(HALL_1) > 100);
+
+  servo[0].write(90);
+  servo[1].write(90);
 
   delay(1000);
 
-  // Servo spins forward at full speed.
-  myservo.write(180);
-  myservo2.write(0);
-  Serial.println("Spinning backwards!");
+  servo[0].write(180);
+  servo[1].write(0);
+
+  int hall_2_read = analogRead(HALL_2);
+  while(250 < hall_2_read && hall_2_read < 550) hall_2_read = analogRead(HALL_2);
   
-  while(!digitalRead(hallSensorPin_2));
-  Serial.println("Stopped!");
-  myservo.write(90);
-  myservo2.write(90);
+  servo[0].write(90);
+  servo[1].write(90);
 
   delay(1000);
 }
-
-
-
-
