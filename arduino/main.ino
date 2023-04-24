f31cb0b83d6ab8025577299554a6d6f4b5b58582
Giorgio Salvemini <s351995@oslomet.no>
2023-04-24 11:16:13 +0000

    Update arduino/main.ino

--- arduino/main.ino
+++ arduino/main.ino
@@ -4,8 +4,11 @@ Servo servo[2];
 
 #define HALL_1 A7 // has led
 #define HALL_2 A6 // does not have led
+
 #define SERVO_1 11
 #define SERVO_2 12
+
+#define PI_OUT 10
 
 void setup() {
   Serial.begin(9600);
@@ -20,27 +23,33 @@ void setup() {
   // Enabling the digital pins on the hall sensor
   pinMode(HALL_1, INPUT);
   pinMode(HALL_2, INPUT);
+
+  digitalWrite(PI_OUT, LOW);
+  pinMode(PI_OUT, OUTPUT);
 }
 
 void loop() {
-  servo[0].write(0);
-  servo[1].write(180);
+  digitalWrite(PI_OUT, HIGH);
+  servo[0].write(83);
+  servo[1].write(97);
 
-  while(analogRead(HALL_1) > 100);
+  int hall_2_read = analogRead(HALL_2);
+  while(250 < hall_2_read && hall_2_read < 900) hall_2_read = analogRead(HALL_2);
 
   servo[0].write(90);
   servo[1].write(90);
 
   delay(1000);
 
-  servo[0].write(180);
-  servo[1].write(0);
+  servo[0].write(97);
+  servo[1].write(83);
 
-  int hall_2_read = analogRead(HALL_2);
-  while(250 < hall_2_read && hall_2_read < 900) hall_2_read = analogRead(HALL_2);
+  while(analogRead(HALL_1) > 100);
   
   servo[0].write(90);
   servo[1].write(90);
 
-  delay(1000);
+  digitalWrite(PI_OUT, LOW);
+
+  delay(300000);
 }
